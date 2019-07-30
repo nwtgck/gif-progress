e3d714cba7723b9e013fda55ab73c15d144b71de
Ryo Ota <nwtgck@gmail.com>
2019-07-30 22:57:11 +0900

    [fix] Make a progress bar move for optimized GIF

--- gif_progress.go
+++ gif_progress.go
@@ -1,6 +1,7 @@
 package gif_progress
 
 import (
+	"image"
 	"image/color"
 	"image/gif"
 )
@@ -9,17 +10,44 @@ func AddProgressBar(inOutGif *gif.GIF, barTop bool, barHeight int, barColor colo
 	// NOTE: inOutGif is changed destructively
 	width := inOutGif.Config.Width
 	height := inOutGif.Config.Height
-	image_len := len(inOutGif.Image)
+	imageLen := len(inOutGif.Image)
+	// Image size
+	imageSize := image.Rect(0, 0, width, height)
+	// Previous frame
+	previousImage := inOutGif.Image[0].SubImage(imageSize)
+	firstPalette := inOutGif.Image[0].Palette
 	for i, paletted := range inOutGif.Image {
-		w := int(float32(width) * ((float32(i)+1)/float32(image_len)))
+		// Create new empty paletted
+		newPaletted := image.NewPaletted(imageSize, firstPalette)
+		// Copy previous frame to the new paletted
+		for x := 0; x < width; x++ {
+			for y := 0; y < height; y++ {
+				newPaletted.Set(x, y, previousImage.At(x, y))
+			}
+		}
+		// Copy whole image to the new paletted
+		rect := paletted.Rect
+		for x := rect.Min.X; x < rect.Max.X; x++ {
+			for y := rect.Min.Y; y < rect.Max.Y; y++ {
+				newPaletted.Set(x, y, paletted.At(x, y))
+			}
+		}
+		// Save as previous
+		previousImage = newPaletted.SubImage(imageSize)
+
+		// Attach progress bar
+		w := int(float32(width) * ((float32(i)+1)/float32(imageLen)))
 		for x := 0; x < w; x++ {
 			for h := 0; h < barHeight; h++ {
 				var y = h
 				if !barTop {
 					y = height - h
 				}
-				paletted.Set(x, y, barColor)
+				newPaletted.Set(x, y, barColor)
 			}
 		}
+
+		// Update image to new paletted
+		inOutGif.Image[i] = newPaletted
 	}
 }
