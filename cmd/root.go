46c52205c65d44e464f4c0e47644b4d94d493c87
JojiiOfficial <jojii@jojii.de>
2020-04-16 13:15:02 +0200

    add custom input/output file flags

--- cmd/root.go
+++ cmd/root.go
@@ -1,24 +1,30 @@
 package cmd
 
 import (
-	gif_progress "github.com/nwtgck/gif-progress"
-	"github.com/spf13/cobra"
-	"gopkg.in/go-playground/colors.v1"
+	"fmt"
 	"image/color"
 	"image/gif"
 	"os"
+	"path/filepath"
+
+	gif_progress "github.com/nwtgck/gif-progress"
+	"github.com/spf13/cobra"
+	"gopkg.in/go-playground/colors.v1"
 )
 
 var barTop bool
 var barHeight int
 var barColorString string
-
+var outFile string
+var inFile string
 
 func init() {
 	cobra.OnInitialize()
-	RootCmd.Flags().BoolVar(&barTop,  "bar-top", false, "Bar is on top")
-	RootCmd.Flags().IntVar(&barHeight,  "bar-height", 5, "Bar height")
-	RootCmd.Flags().StringVar(&barColorString,  "bar-color", "#ccc", "Bar color")
+	RootCmd.Flags().BoolVar(&barTop, "bar-top", false, "Bar is on top")
+	RootCmd.Flags().IntVar(&barHeight, "bar-height", 5, "Bar height")
+	RootCmd.Flags().StringVar(&barColorString, "bar-color", "#ccc", "Bar color")
+	RootCmd.Flags().StringVar(&outFile, "out", "", "Output gif")
+	RootCmd.Flags().StringVar(&inFile, "in", "", "Input gif")
 }
 
 var RootCmd = &cobra.Command{
@@ -26,6 +32,31 @@ var RootCmd = &cobra.Command{
 	Short: "gif-progress",
 	Long:  "Attach progress bar to animated GIF",
 	RunE: func(cmd *cobra.Command, args []string) error {
+		fmt.Println(inFile, outFile)
+
+		r := os.Stdin
+		w := os.Stdout
+
+		if len(inFile) != 0 {
+			// Open file and set source reader to infile
+			f, err := os.Open(filepath.Clean(inFile))
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			r = f
+		}
+
+		if len(outFile) != 0 {
+			// Open file and set dest writer to outfile
+			f, err := os.OpenFile(filepath.Clean(outFile), os.O_TRUNC|os.O_CREATE|os.O_RDWR, 0600)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			w = f
+		}
+
 		// Parse color
 		hex, err := colors.ParseHEX(barColorString)
 		if err != nil {
@@ -33,16 +64,14 @@ var RootCmd = &cobra.Command{
 		}
 		c := hex.ToRGB()
 		barColor := color.RGBA{c.R, c.G, c.B, 255}
-		// TODO: Hard code stdin
-		inOutGif, err := gif.DecodeAll(os.Stdin)
+		inOutGif, err := gif.DecodeAll(r)
 		if err != nil {
 			return err
 		}
 		// Add progress bar to gif
 		gif_progress.AddProgressBar(inOutGif, barTop, barHeight, barColor)
 		// Write gif
-		// TODO: Hard code stdout
-		err = gif.EncodeAll(os.Stdout, inOutGif)
+		err = gif.EncodeAll(w, inOutGif)
 		return err
 	},
 }
